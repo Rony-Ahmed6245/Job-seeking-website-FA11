50253d2c057254158de5fff518a05980bbc5658b
ronyahmed6245 <ronyahmed6245.gmail.com>
2023-11-06 12:24:27 +0600

    env variable api key serect changes

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,15 +2,15 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+console.log(import.meta.env.VITE_P);
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCre7VLwkIxiN00vsyEKHrQzCMzd2P70nE",
-  authDomain: "job-protal-16098.firebaseapp.com",
-  projectId: "job-protal-16098",
-  storageBucket: "job-protal-16098.appspot.com",
-  messagingSenderId: "49159729205",
-  appId: "1:49159729205:web:93ebdc8e0b201b7962417c"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
